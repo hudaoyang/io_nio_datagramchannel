3fb1528adc8b4f638db566c502af63533a9c4853
qjm201000 <1529062902@qq.com>
2019-01-21 13:49:49 +0800

    datagramchannel

---
+++ src/main/java/com/nio/datagramchannel/DatagramchannelApplication.java
@@ -0,0 +1,14 @@
+package com.nio.datagramchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagramchannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatagramchannelApplication.class, args);
+    }
+
+}
+
